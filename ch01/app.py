18c0149846b9d2a3bfbc0d2073a2910035400ce4
zhoulw <645412488@qq.com>
2023-08-23 11:59:12 +0800

    fix python

--- ch01/app.py
+++ ch01/app.py
@@ -1,7 +1,7 @@
- from flask import Flask
- import os
- app = Flask(__name__)
- app.run(debug=True)
- @app.route('/')
- def hello_world():
+from flask import Flask
+import os
+app = Flask(__name__)
+app.run(debug=True)
+@app.route('/')
+def hello_world():
     return 'Hello, my first docker images! ' + os.getenv("HOSTNAME") + ''
