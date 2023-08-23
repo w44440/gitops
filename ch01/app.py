bda93949e52547b0b8704934dff12a511e96a1b7
zhoulw <645412488@qq.com>
2023-08-23 11:29:27 +0800

    ch01 go

---
+++ ch01/app.py
@@ -0,0 +1,7 @@
+ from flask import Flask
+ import os
+ app = Flask(__name__)
+ app.run(debug=True)
+ @app.route('/')
+ def hello_world():
+    return 'Hello, my first docker images! ' + os.getenv("HOSTNAME") + ''
